8e6f0b1f0a2f43f4c9700d9df8b3fff86b88ec64
AnimaTakeshi <33721822+AnimaTakeshi@users.noreply.github.com>
2018-01-02 14:05:56 -0200

    Removed surname field in UserProfile model.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -42,7 +42,6 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    surname = models.CharField(max_length=255)
 
     objects = UserProfileManager()
 
