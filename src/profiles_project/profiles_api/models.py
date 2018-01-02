ab6cb74b16e58fe8337e412db93403e42db58754
AnimaTakeshi <33721822+AnimaTakeshi@users.noreply.github.com>
2018-01-02 13:53:29 -0200

    Added surname field in UserProfile Model.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -42,6 +42,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    surname = models.CharField(max_length=255)
 
     objects = UserProfileManager()
 
